962ccfa73e85af2b6d1249f4a930e4ff407a2ba2
Excitement <graphics@Graphicss-MacBook-Pro.local>
2024-01-17 16:34:11 +0300

    Receipt sample

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import html2pdf from 'html2pdf.js';
+import { Heading, Container, Center, Card, Box, Text, CardHeader, Flex, CardBody, CardFooter, Button, Divider } from '@chakra-ui/react';
 
 
 const generateReferenceNumber = () => {
@@ -13,7 +14,7 @@ const generateReferenceNumber = () => {
 function App() {
   const [referenceNumber, setReferenceNumber] = useState(null);
   const [elapsedTime, setElapsedTime] = useState(null);
-  
+
   const handleCopyReceipt = () => {
     const newReferenceNumber = generateReferenceNumber();
     setReferenceNumber(newReferenceNumber);
@@ -35,9 +36,9 @@ function App() {
     return `${hours}h ${minutes}m ${seconds}s`;
   };
 
-   const generatePDF = () => {
+  const generatePDF = () => {
     const content = document.getElementById('card');
-     const options = {
+    const options = {
       margin: 5,
       filename: 'receipt.pdf',
       image: { type: 'jpeg', quality: 0.98 },
@@ -47,37 +48,108 @@ function App() {
 
     html2pdf().from(content).set(options).save();
   };
- 
-return (
-  <>
 
-    <h1>Copy your Recipt</h1>
-    <div className='container'>
-      
+  return (
+    <>
+      <Container maxW='lg'>
+        <Card >
+          <CardHeader bg='#1850bc' color='white' p={25} borderTopRightRadius={10} borderTopLeftRadius={10}>
+            <Flex dir='row' alignItems='center' justifyContent='space-between' pb={5}>
+              <Box>
+                <Text textAlign='left'>
+                  Amount in
+                </Text>
+              </Box>
+              <Box>
+                <Text fontSize={14} fontWeight='bold'>
+                  Reference No. {referenceNumber}
+                </Text>
+              </Box>
 
-      <div className="card" id='card'>
+            </Flex>
+            <Flex dir='row' alignItems='center' justifyContent='space-between'>
+              <Box>
+                <Text fontSize={20} fontWeight='bold'>
+                  SGD
+                </Text>
+              </Box>
+              <Box>
+                <Text fontSize={20} fontWeight='bold'>
+                  58.00
+                </Text>
+              </Box>
 
-        <p>Reference Number: {referenceNumber}</p>
-        <h1>$75.00</h1>
-        <p>Paid 15 Jan, 2024</p>
+            </Flex>
 
-        <hr />
-        <div>
-            
+          </CardHeader>
+          <CardBody textAlign='left'>
+            <Text>
+              From
+            </Text>
+            <Text fontWeight='bold'>
+              POSB PassBook Savings Account
+            </Text>
+            <Text pb={5}>
+              189-63487-0
+            </Text>
+            <Divider />
+            <Text pt={5}>
+              To
+            </Text>
+            <Text fontWeight='bold'>
+              ANG JIN YI
+            </Text>
+            <Text>
+              Mobile: +6584696523
+            </Text>
+          </CardBody>
+
+          <Flex dir='row' alignItems='center' placeContent='space-between' p={5}>
+            <Box>
+              <Text fontSize={15}>
+                Time elapsed since copy: {elapsedTime}
+              </Text>
+            </Box>
+            <Box alignSelf='right'>
+              <Button variant='outline' onClick={handleCopyReceipt}>Copy</Button>
+            </Box>
+
+          </Flex>
+
+        </Card>
+
+
+
+
+      </Container>
+
+      <div className='container'>
+
+        <div className="card" id='card'>
+
+          <p>Reference Number: {referenceNumber}</p>
+          <h1>$75.00</h1>
+          <p>Paid 15 Jan, 2024</p>
+
+          <hr />
+          <div>
+
+          </div>
+
+
+
+
         </div>
-       
-          
-       
-          
+
+
       </div>
+      {elapsedTime && <p>Time elapsed since copy: {elapsedTime}</p>}
+      <button style={{ color: 'white', background: 'rgb(25, 97, 169)' }} onClick={handleCopyReceipt}>Copy Receipt</button>
 
-      
-    </div>
-    {elapsedTime && <p>Time elapsed since copy: {elapsedTime}</p>}
-     <button style={{ color: 'white', background: 'rgb(25, 97, 169)' }} onClick={handleCopyReceipt}>Copy Receipt</button>
-
-  </>
-);
- };
+    </>
+  );
+};
 
 export default App
+
+
